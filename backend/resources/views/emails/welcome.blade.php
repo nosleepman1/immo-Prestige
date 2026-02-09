2a1e9e8bc8e17c17b2e059118e9232ae669bda08
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-02-09 17:01:28 +0000

    fix mail design

--- backend/resources/views/emails/welcome.blade.php
+++ backend/resources/views/emails/welcome.blade.php
@@ -6,9 +6,9 @@
 </head>
 <body>
     
-    <h1>Bienvenue sur Immo Prestige</h1>
+    <h2>Bienvenue sur Immo Prestige</h2>
     <p>Merci <strong>{{ $name }}</strong> pour votre inscription sur Immo Prestige</p>
-    <p>Vous pouvez désormais vous connecter à votre compte en utilisant votre email <strong>{{ $email }}</strong> et votre mot de passe</p>
+   
     <p>Si vous avez des questions, n'hésitez pas à nous contacter</p>
     <p>Email: <a href="mailto:contact@immo-prestige.com">contact@immo-prestige.com</a></p>
     <p>Téléphone: <a href="tel:+221773757077">+221 77 375 70 77</a></p>
