5dabb646ca48c381f8b7d896e321d29331c5baa7
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-02-27 07:28:25 +0000

    add providers (theme and authProvider for protected routes )

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,55 @@
 import Navbar from "./components/Navbar"
-import { ThemeProvider } from "./context/ThemeContext"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Login from "./pages/auth/Login"
 import Register from "./pages/auth/Register"
+import AddProperty from "./pages/properties/AddProperty"
+import Loader from "./components/Loader"
+import Profile from "./pages/auth/Profile"
+import PrivateRoute from "./routes/PrivateRoutes"
 
 function App() {
   
 
   return (
-    <ThemeProvider>
+  
       <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<div className="p-4">Welcome to Immo Prestige</div>} />
-          <Route path="/properties" element={<div className="p-4">Properties Page</div>} />
-          <Route path="/agents" element={<div className="p-4">Agents Page</div>} />
-          <Route path="/contact" element={<div className="p-4">Contact Page</div>} />
 
+        <Routes>
+          
+          <Route path="/" element={<Loader />} />
+
+          
+            <Route path="/properties" element={
+              <PrivateRoute>
+                <div className="p-4">Properties Page</div>
+              </PrivateRoute>
+            } />
+            
+            
+            <Route path="/agents" element={
+              <PrivateRoute>
+                <div className="p-4">Agents Page</div>
+              </PrivateRoute>
+            } />
+            
+            
+            <Route path="/contact" element={<div className="p-4">Contact Page</div>} />
+           
+            <Route path="/profile" element={
+              <PrivateRoute>
+                <Profile />
+              </PrivateRoute>
+            } />
+          
           
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/register-agency" element={<RegisterAgency />} />
+        
         </Routes>
       </Router>
-    </ThemeProvider>
+
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from './context/AuthProvider.jsx'
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ThemeProvider >
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </ThemeProvider>
 )
